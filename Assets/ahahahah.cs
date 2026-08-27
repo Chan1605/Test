d9d03d7858566af98c2a3f0a902fca4f0b15e7bc
Chan1605 <105186869+Chan1605@users.noreply.github.com>
2026-08-27 15:15:44 +0900

    Update ahahahah.cs

--- Assets/ahahahah.cs
+++ Assets/ahahahah.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class ahahahah : MonoBehaviour
 {
+    [SerializeField]private float RotateSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RotateSpeed = 50f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.Rotate(0f, RotateSpeed * Time.deltaTime, 0f);
     }
 }
